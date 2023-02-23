5a1ca0c2a4390584099064750437da6f2e546057
Helen Alves dos Reis <helen.reis@mrv.com.br>
2023-02-22 21:58:09 -0300

    updating the big picture to last version

--- docs/big_picture/big_picture_code_broker.py
+++ docs/big_picture/big_picture_code_broker.py
@@ -8,21 +8,25 @@ with Diagram("Event Driven - Broker Topology", show=False, filename="big_picture
         with Cluster("cooking_container"):
             cooking_service = AppServices("CookingService")
         with Cluster("event_queue_container"):
-            queue = RabbitMQ("event queue")
+            queue = RabbitMQ("bread queue")
         with Cluster("bread_container"):
             bread = AppServices("BreadService")
-            db_bread = Custom("db_bread", "../sql_lite.png")
+            #db_bread = Custom("db_bread", "../sql_lite.png")
+        with Cluster("event_queue_container"):
+            cheese_queue = RabbitMQ("cheese queue")
         with Cluster("cheese_container"):
             cheese = AppServices("CheeseService")
-            db_cheese = Custom("db_cheese", "../sql_lite.png")
+            #db_cheese = Custom("db_cheese", "../sql_lite.png")
+        with Cluster("event_queue_container"):
+            steak_queue = RabbitMQ("steak queue")
         with Cluster("steak_container"):
             steak = AppServices("SteakService")
-            db_steak = Custom("db_steak", "../sql_lite.png")
+            #db_steak = Custom("db_steak", "../sql_lite.png")
             
-    bread >> db_bread
-    cheese >> db_cheese
-    steak >> db_steak
+    #bread >> db_bread
+    #cheese >> db_cheese
+    #steak >> db_steak
 
     workers = [bread, cheese, steak]
     
-    cooking_service >> queue >> steak >> cheese >> bread
+    cooking_service >> queue >> bread >> cheese_queue >> cheese >> steak_queue >> steak
--- docs/big_picture/big_picture_code_mediator.py
+++ docs/big_picture/big_picture_code_mediator.py
@@ -8,25 +8,18 @@ with Diagram("Event Driven - Mediator Topology", show=False, filename="big_pictu
         with Cluster("cooking_container"):
             cooking_service = AppServices("CookingService")
         with Cluster("event_queue_container"):
-            queue = RabbitMQ("event queue")
+            queue = RabbitMQ("cooking_queue")
         with Cluster("bread_container"):
             bread = AppServices("BreadService")
-            db_bread = Custom("db_bread", "../sql_lite.png")
         with Cluster("cheese_container"):
             cheese = AppServices("CheeseService")
-            db_cheese = Custom("db_cheese", "../sql_lite.png")
         with Cluster("steak_container"):
             steak = AppServices("SteakService")
-            db_steak = Custom("db_steak", "../sql_lite.png")
         with Cluster("event_queue_waiter_container", direction="LR"):
-            waiter_queue = RabbitMQ("event queue")
+            waiter_queue = RabbitMQ("waiter_queue")
         with Cluster("waiter_service", direction="LR"):
             waiter = AppServices("WaiterService")
             db_waiter = Custom("db_waiter", "../sql_lite.png")
-            
-    bread >> db_bread
-    cheese >> db_cheese
-    steak >> db_steak
 
     workers = [bread, cheese, steak]
     
